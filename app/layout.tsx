e2b8303c38186a9fba53dbcdc04d488514ef0e26
변재성 <common@sillaenc.co.kr>
2024-08-22 16:22:00 +0900

    project create!!!!

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "linux 정리 blog",
+  description: "next.js + typescript + mongodb로 작성.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
